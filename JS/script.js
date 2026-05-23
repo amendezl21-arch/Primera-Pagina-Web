ecf3f5d5fc9b271879a24aa82854748afab2512f
Allison <amendezl21@miumg.edu.gt>
2026-05-23 02:22:42 -0600

    first commit

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+$('.bxslider').bxSlider({
+    auto: true,
+    pause: 3500,
+    speed: 600,
+    slideWidth: 900,
+    adaptiveHeight: false,
+    mode: 'fade',
+    pager: true,
+    controls: true,
+    prevText: '‹',
+    nextText: '›'
+});
